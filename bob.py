302f315fb38829a3369d73580671c44f1bd72634
pro-thorin <alex.jiao5@gmail.com>
2019-11-12 08:33:58 -0500

    Fixed some things

--- bob.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from ev3_robot import Ev3Robot
-move = Ev3Robot()
-move.move_in_cm(centimeters = 41, speed = 20)
--- ev3_robot.py
+++ ev3_robot.py
@@ -9,8 +9,8 @@ class Ev3Robot:
         self.steer_pair = MoveSteering(wheel1, wheel2)
         self.gyro = GyroSensor()
         self.tank_pair = MoveTank(wheel1, wheel2)
-        self.motorB = LargeMotor(OUTPUT_B)
-        self.motorC = LargeMotor(OUTPUT_C)
+        self.motor1 = LargeMotor(wheel1)
+        self.motor2 = LargeMotor(wheel2)
 
     def pivot_right(self, degrees, speed = 20):
         self.tank_pair.on(left_speed = 0, right_speed = speed)
@@ -35,7 +35,6 @@ class Ev3Robot:
     def spin_left(self, degrees, speed = 20):
         self.gyro.mode = 'GYRO-ANG'
         value1 = self.gyro.angles
-        self.gyro.reset()
         self.tank_pair.on(left_speed = speed * -1, right_speed = speed)
         self.gyro.wait_until_angle_changed_by(degrees)
         self.tank_pair.off()
@@ -44,11 +43,10 @@ class Ev3Robot:
         self.gyro.wait_until_angle_changed_by(value2 - value1 - degrees)
 
     def go_straight_forward(self, cm, speed = 30):
-        value1 = self.motorB.position
+        value1 = self.motor1.position
         angle0 = self.gyro.angle
         rotations = cm / 19.05
-        while (self.motorB.position - value1) / 360 < rotations:
+        while (self.motor1.position - value1) / 360 < rotations:
             self.gyro.mode = 'GYRO-ANG'
             angle = self.gyro.angle - angle0
-            print(angle, file = stderr)
             self.steer_pair.on(steering = angle * -1, speed = speed)
