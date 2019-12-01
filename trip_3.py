5cf10f023db6e31c8b988806c50d76aa0198b8b1
Jayden Ai <jaydenai@Chunmeis-Air.home>
2019-12-01 10:55:24 -0500

    Created Trip 3

--- trip_3.py
+++ trip_3.py
@@ -4,14 +4,16 @@ from ev3dev2.motor import SpeedDPS, SpeedRPS, SpeedRPM
 from ev3dev2.sensor.lego import GyroSensor
 from time import sleep
 from ev3_robot import Ev3Robot
-
 tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
 gyro = GyroSensor()
-
 robot = Ev3Robot()
-robot.go_straight_forward(cm = 70, speed = 30)
-robot.go_straight_forward(cm = 10, speed = 7.5)
-robot.go_straight_forward(cm = -40, speed = 30)
-robot.spin_right(degrees = 90, speed = 20)
-robot.go_straight_forward(cm = -60, speed = 30)
-
+robot.go_straight_forward(cm = 46, speed = -30)
+robot.go_straight_forward(cm = 5, speed = -40)
+robot.spin_left(degrees = 0.9)
+robot.go_straight_forward(cm = 20, speed = -30)
+robot.pivot_left(degrees = 60, speed = 20)
+motor1.off
+motor2.off
+sleep(1)
+robot.pivot_right(degrees = 60, speed = 20)
+robot.go_straight_forward(cm = 30, speed = -30)
