381dce1b26ebcc2cca66dae083997e52a66269f0
Jayden Ai <jaydenai@Chunmeis-Air.home>
2019-12-01 13:57:09 -0500

    trip3 changed

--- trip_3.py
+++ trip_3.py
@@ -10,20 +10,26 @@ gyro = GyroSensor()
 robot = Ev3Robot()
 motor1 = LargeMotor(OUTPUT_B)
 motor2 = LargeMotor(OUTPUT_C)
-
-robot.go_straight_forward(cm = 46, speed = -30)
+#solving mission 6, traaffic jam
+robot.go_straight_forward(cm = 65, speed = -30)
 robot.go_straight_forward(cm = 5, speed = -40)
 robot.spin_left(degrees = 0.9)
 robot.go_straight_forward(cm = 20, speed = -30)
+#solving mission 7, swing
 robot.pivot_left(degrees = 60, speed = 20)
 motor1.off
 motor2.off
 sleep(1)
 robot.go_straight_forward(cm = 10, speed = -20)
-robot.pivot_right(degrees = 60, speed = 20)
+robot.pivot_right(degrees = 52, speed = 20)
 motor1.off
 motor2.off
 sleep(1)
 robot.go_straight_forward(cm = 20, speed = -20)
 robot.go_straight_forward(cm = 30, speed = -30)
-robot.go_straight_backward(cm = 200, speed = -80)
+robot.go_straight_backward(cm = 20, speed = -30)
+robot.spin_left(degrees = 21.9)
+motor1.off
+motor2.off
+sleep(0.5)
+robot.go_straight_forward(cm = 48, speed = -30)
