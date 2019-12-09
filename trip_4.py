ae3bc4a7da6a3df19d11f874ca870f5cb4d51b49
pro-thorin <alex.jiao5@gmail.com>
2019-12-08 19:23:35 -0500

    Updated it

--- trip_4.py
+++ trip_4.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env micropython
 from ev3_robot import Ev3Robot
 robot = Ev3Robot()
-robot.go_straight_forward(centimeters = 41, speed = 20)
+robot.go_straight_forward(cm = 66, speed = -20)
+robot.go_straight_backward(cm = 100, speed = -50)
