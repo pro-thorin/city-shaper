c6f2a0ee2aaefa1f34d65352a0f351b61bc5e154
Jayden Ai <jaydenai@Chunmeis-Air.home>
2019-12-31 13:01:40 -0500

    Useless "bob is the coolest" deleted

--- master_program.py
+++ master_program.py
@@ -60,4 +60,3 @@ while not button.any():
 # #solving mission 11 & 12, innovative architecture/design and build
 # robot.go_straight_forward(cm = 66, speed = -20)
 # robot.go_straight_backward(cm = 100, speed = -50)
-print("Bob is the coolest!", file = stderr)
