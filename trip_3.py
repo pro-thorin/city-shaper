8d5cce7f2440d849ae89693ce717bb316b6d0965
Jayden Ai <jaydenai@Chunmeis-Air.home>
2019-12-31 10:48:51 -0500

    trip_3 revised

--- trip_3.py
+++ trip_3.py
@@ -5,7 +5,7 @@ from ev3dev2.sensor.lego import GyroSensor
 from time import sleep
 from ev3_robot import Ev3Robot
 
-tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
+tank_pair = MoveTank(OUTPUT_B, OUTPUT_C
 gyro = GyroSensor()
 robot = Ev3Robot()
 motor1 = LargeMotor(OUTPUT_B)
