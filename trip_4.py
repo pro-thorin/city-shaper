95e0c593fbf904e0d58a447ebdcc81f16ea5bc2b
Jayden Ai <jaydenai@Chunmeis-Air.home>
2020-01-01 10:51:31 -0500

    4 is now cool

--- trip_4.py
+++ trip_4.py
@@ -10,12 +10,12 @@ def trip4():
     robot.go_straight_forward(cm = 66, speed = -20)
     robot.go_straight_backward(cm = 10, speed = -50)
     robot.spin_left(degrees = 70)
-    robot.go_straight_backward(cm = 25, speed = -40)
+    robot.go_straight_forward(cm = 35, speed = -40)
     robot.align(15)
     robot.spin_right(degrees = 90)
     robot.go_straight_forward(cm = 50, speed = -40)
     robot.spin_right(degrees = 45)
-    robot.go_straight_backward(cm = 80, speed = -70)
+    robot.go_straight_backward(cm = 90, speed = -70)
 
 if __name__ == '__main__':
     trip4()
