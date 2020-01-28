e985db16ceaa4ddcc89719f8b54a502a0210e113
Jayden Ai <jaydenai@Chunmeis-Air.home>
2020-01-27 19:53:54 -0500

    ramp change

--- ramp.py
+++ ramp.py
@@ -6,9 +6,9 @@ robot = Ev3Robot()
 robot.go_straight_backward(30, speed=30)
 robot.go_straight_backward(15, speed=20)
 robot.go_straight_forward(7)
-robot.go_straight_backward(20, speed=45)
+robot.go_straight_backward(20, speed=100)
 robot.spin_left(30)
-robot.go_straight_backward(120, speed=35)
+robot.go_straight_backward(120, speed=36)
 sleep(1)
 
 while True:
