7a5fcbb72e2c9cfa90c78dea57229ecc56efe069
Jayden Ai <jaydenai@Chunmeis-Air.home>
2020-01-27 19:37:37 -0500

    ramp.py is now a new thing

---
+++ ramp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env micropython
+from ev3_robot import Ev3Robot
+from time import sleep
+robot = Ev3Robot()
+
+robot.go_straight_backward(30, speed=30)
+robot.go_straight_backward(15, speed=20)
+robot.go_straight_forward(7)
+robot.go_straight_backward(20, speed=45)
+robot.spin_left(30)
+robot.go_straight_backward(120, speed=35)
+sleep(1)
+
+while True:
+    robot.stop()
