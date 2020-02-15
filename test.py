c0ef1917450fdfc319297ae0e92e549292fb6f8c
pro-thorin <alex.jiao5@gmail.com>
2020-02-15 09:16:29 -0500

    👊

--- test.py
+++ test.py
@@ -17,19 +17,3 @@ robot = Ev3Robot()
 # while True: 
 #     robot.stop()
 
-
-
-robot.spin_right(35)
-robot.go_straight_forward(65.5)
-robot.spin_right(8, 25)
-robot.go_straight_backward(3.5)
-
-robot.dog_gear(motor = 4, degrees = 135, speed = -30)
-robot.go_straight_forward(2)
-robot.spin_left(15)
-robot.dog_gear(degrees = 405, motor = 4, speed = 10)
-robot.spin_right(29)
-
-robot.go_straight_forward(10, 5)
-robot.dog_gear(degrees = 200, motor = 3, speed = -10)
-
--- trip_1.py
+++ trip_1.py
@@ -4,22 +4,32 @@ from ev3_robot import Ev3Robot
 
 #Defining
 def trip1():
+    # hangs the bat on the tree
     robot = Ev3Robot()
-    #push the lever up to release the blue block
-    robot.go_straight_forward(cm=17, speed=30)
+    robot.spin_right(35)
+    robot.go_straight_forward(65.5)
+    robot.spin_right(8, 25)
+    robot.go_straight_backward(3.5)
+    # deposits the blue block into the treehouse
+    robot.dog_gear(motor = 4, degrees = 135, speed = -30)
+    robot.go_straight_forward(2)
+    robot.spin_left(15)
+    robot.dog_gear(degrees = 405, motor = 4, speed = 40)
+    robot.spin_right(29)
+    robot.go_straight_forward(9, 5)
+    robot.dog_gear(degrees = 250, motor = 3, speed = -10)
+    # raises the lever on the crane, releasing the blue block 
+    robot.go_straight_backward(43)
+    robot.spin_left(64)
+    robot.go_straight_forward(27)
     sleep(1)
-    robot.spin_right(46)
-    robot.go_straight_forward(cm = 55)
-    robot.align(15)
-    robot.go_straight_forward(cm = 7)
-    robot.spin_right(degrees = 15)
-
-    robot.dog_gear(degrees = 180, speed = 50)
-    # robot.go_straight_backward(cm=18, speed=30)
-    # robot.spin_right(degrees= 90, speed=20)
-    # #go back to home
-    # robot.go_straight_backward(cm = 65, speed=70)
-
+    robot.go_straight_backward(3)
+    robot.pivot_right(degrees = 65)
+    robot.go_straight_backward(10)
+    robot.spin_left(50)
+    robot.go_straight_forward(27)
+    robot.spin_left(90)
+    robot.go_straight_forward(27)
 
 if __name__ == '__main__':
-    trip1
+    trip1()
